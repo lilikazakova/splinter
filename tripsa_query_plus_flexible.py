5f6becd70b3b13518a19f810f9f6d570ef490547
lilikazakova <lilikazakova@gmail.com>
2014-05-09 12:40:04 +0300

    sends email with flight info plus cheapest flight
    Sends email for the flight on the exact date plus info for the cheapest
    flight +-3 days

--- tripsa_query_plus_flexible.py
+++ tripsa_query_plus_flexible.py
@@ -59,46 +59,59 @@ class TestSequenceFunctions(unittest.TestCase):
         time.sleep(10)
         self.make_screenshot() 
         
-        print browser.find_by_css('div.matrix-tooltip a').text.split
-
-        #price = browser.find_by_css('span.amount').first.text[1:]
-        #time_departure = browser.find_by_css('td.depart span.cursor_default').first.text
-        #time_arrival = browser.find_by_css('td.arrive span.cursor_default').first.text
-        #self.make_screenshot() 
+        price = browser.find_by_css('td.selected-price div.matrix-tooltip a').text[1:]
+        lowest_price = browser.find_by_css('td.cheapest-price div.matrix-tooltip a').first.text[1:]
         
-        #message = 'Flight from %s  to %s  Departure on %s at %s Arrive at %s Lowest price %s' %(self.location, #self.destination, self.departure, time_departure, time_arrival, price)
+        browser.find_by_css('td.selected-price div.matrix-tooltip a').mouse_over()
+        dep_date_price = browser.find_by_css('td.selected-price div.matrix-tooltip div.body div.block strong').first.text
+        company_price = browser.find_by_css('td.selected-price div.matrix-tooltip div.body div.comment').text
+        dep_time_price = browser.find_by_css('td.selected-price div.matrix-tooltip div.body div.small').first.text
+        return_date_price = browser.find_by_css('td.selected-price div.matrix-tooltip div.body div.block strong').last.text
+        return_time_price = browser.find_by_css('td.selected-price div.matrix-tooltip div.body div.small').last.text
+        self.make_screenshot() 
+        
+        browser.find_by_css('td.cheapest-price div.matrix-tooltip a').mouse_over()
+        dep_date_lowest_price = browser.find_by_css('td.cheapest-price div.matrix-tooltip div.body div.block strong').first.text
+        company_lowest_price = browser.find_by_css('td.cheapest-price div.matrix-tooltip div.body div.comment').text
+        dep_time_lowest_price = browser.find_by_css('td.cheapest-price div.matrix-tooltip div.body div.small').first.text
+        return_date_lowest_price = browser.find_by_css('td.cheapest-price div.matrix-tooltip div.body div.block strong')[1].text
+        return_time_lowest_price = browser.find_by_css('td.cheapest-price div.matrix-tooltip div.body div.small')[1].text
+        self.make_screenshot() 
+        message_price = 'Price: %s \n %s \n %s \n %s \n %s \n %s \n' %(price, dep_date_price, dep_time_price, company_price, return_date_price, return_time_price)
+        message_lowest_price = 'Lowest price: %s \n %s \n %s \n %s \n %s \n %s' %(lowest_price, dep_date_lowest_price, dep_time_lowest_price, company_lowest_price, return_date_lowest_price, return_time_lowest_price)
+        message = 'Available Flight from %s  to %s : \n \n %s \n First cheapest flight: \n %s' %(self.location, self.destination, message_price, message_lowest_price)
         
         #browser.find_by_id('form-flights-result-0')
                 
-        #browser.visit(self.mail_url)
-        #self.make_screenshot()
+        browser.visit(self.mail_url)
+        self.make_screenshot()
 
-        #browser.find_by_name('username').type(self.user)
-        #self.make_screenshot()
+        browser.find_by_name('username').type(self.user)
+        self.make_screenshot()
         
-        #time.sleep(5)
-        #self.make_screenshot()         
+        time.sleep(5)
+        self.make_screenshot()         
 
-        #browser.find_by_name('password').type(self.passwd)
-        #self.make_screenshot()
+        browser.find_by_name('password').type(self.passwd)
+        self.make_screenshot()
 
-        #browser.find_by_css('input.submit').click()
-        #self.make_screenshot()
+        browser.find_by_css('input.submit').click()
+        self.make_screenshot()
 				
-        #browser.find_by_css('a.write').click()
-        #self.make_screenshot()
+        browser.find_by_css('a.write').click()
+        self.make_screenshot()
 		
-        #browser.find_by_name('TO').type(self.address)
-        #self.make_screenshot()	
+        browser.find_by_name('TO').type(self.address)
+        self.make_screenshot()	
 
-        #browser.find_by_name('SUBJECT').type(self.subject)
-        #self.make_screenshot()	
+        browser.find_by_name('SUBJECT').type(self.subject)
+        self.make_screenshot()	
 
-        #browser.find_by_name('BODY').type(message)
-        #self.make_screenshot()	
+        browser.find_by_name('BODY').type(message)
+        self.make_screenshot()	
 
-        #browser.find_by_name('SENDMESSAGE_SEND').click()
-        #self.make_screenshot()		
+        browser.find_by_name('SENDMESSAGE_SEND').click()
+        self.make_screenshot()		
         
 if __name__ == '__main__':
     unittest.main()
